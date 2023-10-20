4844555ad93c20a4ed3cf65bcacd6959241e2f3c
amandakenes <amandakeness@outlook.com>
2023-10-20 12:59:31 -0300

    add footer + js functions

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 function bgChangeLight() {
-  document.body.style.background = "#ffff";
-  document.getElementById("content-paragraph").style.color = "#000000";
+  document.body.style.background = "white";
+  document.getElementById("content-paragraph").style.color = "black";
 }
 
 function bgChangeDark() {
   document.body.style.background = "#222121f5";
   document.getElementById("content-paragraph").style.color = "white";
-
 }
