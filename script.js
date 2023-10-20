7d01fdb7bae5dc781a5cbac671b9a7cf77e5e7d0
amandakenes <amandakeness@outlook.com>
2023-10-20 09:51:25 -0300

    js functions, color and id changes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function bgChange() {
+function bgChangeLight() {
   document.body.style.background = "#ffff";
+  document.getElementById("content-paragraph").style.color = "#000000";
+}
+
+function bgChangeDark() {
+  document.body.style.background = "#222121f5";
+  document.getElementById("content-paragraph").style.color = "white";
+
 }
